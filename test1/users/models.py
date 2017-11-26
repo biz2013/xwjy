4910b0220aac291fcc0be636b52ac7bad25786bf
root <tttzhang2000@yahoo.com>
2017-11-26 14:22:19 -0800

    fix database script

--- test1/users/models.py
+++ test1/users/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 
+class UserLogin(models.Model):
+   username = models.CharField(max_length=32, primary_key=True)
+   passwd_hash = models.CharField(max_length=64)
+   alias = models.CharField(max_length=64)
+   config_json = models.CharField(max_length=4096)
+   created_at = models.DateTimeField(auto_now_add=True)
+   created_by = models.ForeignKey('self', on_delete = models.CASCADE, related_name="login_created_by")
+   lastupdated_at = models.DateTimeField(auto_now=True)
+   lastupdated_by = models.ForeignKey('self', on_delete=models.CASCADE, related_name="login_lastupdated_by")
+
 class PaymentProvider(models.Model):
    name = models.CharField(max_length=32)
    config_json = models.TextField()
    created_at = models.DateTimeField(auto_now_add=True)
-   created_by = models.IntegerField()
+   created_by = models.ForeignKey('UserLogin', related_name='PaymentProvider_created_by')
    lastupdated_at = models.DateTimeField(auto_now=True)
-   lastupdated_by = models.IntegerField()
+   lastupdated_by = models.ForeignKey('UserLogin', related_name='PaymentProvider_lastupdated_by')
 
 class User(models.Model):
+   login = models.OneToOneField('UserLogin', on_delete = models.CASCADE,null=True)
    firstname = models.CharField(max_length=32)
    middle = models.CharField(max_length=32)
    lastname = models.CharField(max_length=32)
@@ -16,20 +27,9 @@ class User(models.Model):
    phone = models.CharField(max_length=32)
    config_json = models.TextField()
    created_at = models.DateTimeField(auto_now_add=True)
-   created_by = models.IntegerField()
+   created_by = models.ForeignKey('UserLogin', related_name='User_created_by')
    lastupdated_at = models.DateTimeField(auto_now=True)
-   lastupdated_by = models.IntegerField()
-
-class UserLogin(models.Model):
-   userId = models.OneToOneField('User', on_delete = models.CASCADE)
-   username = models.CharField(max_length=32, primary_key=True)
-   passwd_hash = models.CharField(max_length=64)
-   alias = models.CharField(max_length=64)
-   config_json = models.CharField(max_length=4096)
-   created_at = models.DateTimeField(auto_now_add=True)
-   created_by = models.IntegerField()
-   lastupdated_at = models.DateTimeField(auto_now=True)
-   lastupdated_by = models.IntegerField()
+   lastupdated_by = models.ForeignKey('UserLogin', related_name='User_lastupdated_by')
 
 class UserPaymentMethod(models.Model):
    userId = models.ForeignKey('User', on_delete=models.CASCADE)
@@ -37,26 +37,26 @@ class UserPaymentMethod(models.Model):
    account_at_provider= models.CharField(max_length=64)
    provider_qrcode_image = models.ImageField()
    created_at = models.DateTimeField(auto_now_add=True)
-   created_by = models.IntegerField()
+   created_by = models.ForeignKey('UserLogin', related_name='UserPaymentMethod_created_by')
    lastupdated_at = models.DateTimeField(auto_now=True)
-   lastupdated_by = models.IntegerField()
+   lastupdated_by = models.ForeignKey('UserLogin', related_name='UserPaymentMethod_lastupdated_by')
 
 
 class Cryptocurrency(models.Model):
    currency_code = models.CharField(max_length=16, primary_key=True)
    name = models.CharField(max_length=32)
    created_at = models.DateTimeField(auto_now_add=True)
-   created_by = models.IntegerField()
+   created_by = models.ForeignKey('UserLogin', related_name='Cryptocurrency_created_by')
    lastupdated_at = models.DateTimeField(auto_now=True)
-   lastupdated_by = models.IntegerField()
+   lastupdated_by = models.ForeignKey('UserLogin', related_name='Cryptocurrency_lastupdated_by')
 
 class Wallet(models.Model):
    cryptocurrent_code = models.ForeignKey('Cryptocurrency', on_delete=models.CASCADE)
    config_json = models.TextField()
    created_at = models.DateTimeField(auto_now_add=True)
-   created_by = models.IntegerField()
+   created_by = models.ForeignKey('UserLogin', related_name='Wallet_created_by')
    lastupdated_at = models.DateTimeField(auto_now=True)
-   lastupdated_by = models.IntegerField()
+   lastupdated_by = models.ForeignKey('UserLogin', related_name='Wallet_lastupdated_by')
 
 
 class UserWallet(models.Model):
@@ -64,9 +64,9 @@ class UserWallet(models.Model):
    wallet = models.ForeignKey('Wallet', on_delete=models.CASCADE)
    wallet_addr = models.CharField(max_length=128)
    created_at = models.DateTimeField(auto_now_add=True)
-   created_by = models.IntegerField()
+   created_by = models.ForeignKey('UserLogin', related_name='UserWallet_created_by')
    lastupdated_at = models.DateTimeField(auto_now=True)
-   lastupdated_by = models.IntegerField()
+   lastupdated_by = models.ForeignKey('UserLogin', related_name='UserWallet_lastupdated_by')
 
 class UserReview(models.Model):
    target_userId = models.ForeignKey('User', on_delete=models.CASCADE, related_name='target_user')
@@ -74,18 +74,18 @@ class UserReview(models.Model):
    score = models.FloatField()
    message = models.TextField()
    created_at = models.DateTimeField(auto_now_add=True)
-   created_by = models.IntegerField()
+   created_by = models.ForeignKey('UserLogin', related_name='UserReview_created_by')
    lastupdated_at = models.DateTimeField(auto_now=True)
-   lastupdated_by = models.IntegerField()
+   lastupdated_by = models.ForeignKey('UserLogin', related_name='UserReview_lastupdated_by')
 
 class UserStatue(models.Model):
    user = models.ForeignKey('User', on_delete=models.CASCADE)
    avg_score = models.FloatField()
    trans_count = models.IntegerField()
    created_at = models.DateTimeField(auto_now_add=True)
-   created_by = models.IntegerField()
+   created_by = models.ForeignKey('UserLogin', related_name='UserStatue_created_by')
    lastupdated_at = models.DateTimeField(auto_now=True)
-   lastupdated_by = models.IntegerField()
+   lastupdated_by = models.ForeignKey('UserLogin', related_name='UserStatue_lastupdated_by')
 
 class Order(models.Model):
    ORDER_TYPE = (('BUY','Buy'),('SELL','Sell'))
@@ -102,9 +102,9 @@ class Order(models.Model):
    unit_price_currency = models.CharField(max_length = 8, choices=CURRENCY, default='CYN')
    status = models.CharField(max_length=32)
    created_at = models.DateTimeField(auto_now_add=True)
-   created_by = models.IntegerField()
+   created_by = models.ForeignKey('UserLogin', related_name='Order_created_by')
    lastupdated_at = models.DateTimeField(auto_now=True)
-   lastupdated_by = models.IntegerField()
+   lastupdated_by = models.ForeignKey('UserLogin', related_name='Order_lastupdated_by')
 
 class OrderChangeLog(models.Model):
    order = models.ForeignKey('Order', on_delete=models.CASCADE)
