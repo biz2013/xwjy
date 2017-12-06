c3a0564c4ea7cde0cbeffec19973fbc484ffba9a
Tao <taozhang@salesforce.com>
2017-12-06 12:04:36 -0800

    fix script

--- test1/static/scripts/purchase_ui.js
+++ test1/static/scripts/purchase_ui.js
@@ -1,8 +1,8 @@
 function create_purchase_order(sell_order_id, sell_order_owner_userid,
    locked_in_unit_price, available_units_for_purchase) {
-    (#reference_order_id).value = sell_order_id;
-    (#owner_user_id).value = sell_order_owner_userid;
-    (#locked_in_unit_price).value = locked_in_unit_price;
-    (#available_units_for_purchase).value = available_units_for_purchase
-    (#form_create_purchase_order).submit();
+    $("#reference_order_id").val(sell_order_id);
+    $("#owner_user_id").val(sell_order_owner_userid);
+    $("#locked_in_unit_price").val(locked_in_unit_price);
+    $("#available_units_for_purchase").val(available_units_for_purchase);
+    $("#form_create_purchase_order").submit();
 }
