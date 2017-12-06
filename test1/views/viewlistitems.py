74198a675ff71ceaa7307b1d8aed38390b0b916b
Tao <taozhang@salesforce.com>
2017-12-05 22:19:58 -0800

    add input for purchase order

--- test1/views/viewlistitems.py
+++ test1/views/viewlistitems.py
@@ -1,14 +1,14 @@
 
 class OrderViewListItem(object):
-   def __init__(login, userid, status, units, unit_price, 
-       unit_price, unit_price_currency, unit_balance, 
-       unit_avaiable, lastupdated_at):
-       self.user_login = login
-       self.user_id = userid
+   def __init__(order_owner_login, userid, status, units, unit_price,
+       unit_price, unit_price_currency, unit_balance,
+       available_units, lastupdated_at):
+       self.owner_login = order_owner_login
+       self.owner_user_id = userid
        self.status = status
        self.units = units
        self.unit_price = unit_price
-       self.unit_price_currency= unit_price_currency       
+       self.unit_price_currency= unit_price_currency
        self.unit_balance = unit_balance
-       self.unit_available = unit_available
-       self.lastupdated_at = lastupdated_at 
+       self.available_units = available_units
+       self.lastupdated_at = lastupdated_at
