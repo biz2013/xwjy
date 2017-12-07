c716bc3b43678f9cfa9875423bca4a12b5acebdf
Tao <taozhang@salesforce.com>
2017-12-06 17:55:50 -0800

    fix ui jquery selector

--- test1/static/scripts/purchase_ui.js
+++ test1/static/scripts/purchase_ui.js
@@ -4,5 +4,12 @@ function create_purchase_order(sell_order_id, sell_order_owner_userid,
     $("#owner_user_id").val(sell_order_owner_userid);
     $("#locked_in_unit_price").val(locked_in_unit_price);
     $("#available_units_for_purchase").val(available_units_for_purchase);
+    alert("message")
     $("#form_create_purchase_order").submit();
+}
+
+function show_total_cost(units, price) {
+  if (isNaN(units)) {
+    $("#total_cost").text(units * price);
+  }
 }
