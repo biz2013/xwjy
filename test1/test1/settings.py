381a17d562fdd6a26df8eb3a708500975f509f60
Tao <taozhang@salesforce.com>
2017-12-18 19:01:58 -0800

    create singleton site config

--- test1/test1/settings.py
+++ test1/test1/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'mathfilters',
     'views',
     'users',
+    'config',
     'controller',
 ]
 
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'config.context_processor.settings',
             ],
         },
     },
