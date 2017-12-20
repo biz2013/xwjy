589bc81407b1e784e55332cab03114b435448213
Tao <taozhang@salesforce.com>
2017-12-19 17:55:06 -0800

    Finally bring up the header

--- test1/users/sitesettings.py
+++
@@ -1,7 +0,0 @@
-from .singletonmodel SingletonModel
-
-class SiteSettings(SingletonModel):
-    support = models.EmailField(default='support@example.com')
-    heepay_notify_url_host = models.CharField(default='localhost')
-    heepapy_notify_url_port = models.IntegerField(default=8000)
-    
