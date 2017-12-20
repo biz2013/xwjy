ee1af525a1e25094a57c9b42e24cb37ddc8081d2
Tao <taozhang@salesforce.com>
2017-12-19 20:15:44 -0800

    small fixes

--- test1/views/errorpage.py
+++ test1/views/errorpage.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 from django.db.models import Q
 from django.shortcuts import render, redirect
+from controller.global_constants import *
 
 def show_error(request, error_level, error_msg):
-    return render(request, 'html/error_page.html',
-                  { 'error_level' : error_level,
-                    'error_msg': error_msg})
+    if error_level == ERR_CRITICAL_IRRECOVERABLE:
+        request.session[REQ_KEY_USERNAME] = None
+        request.session[REQ_KEY_USERID] = None
+    return render(request, 'html/error_page.html', { 'error_msg': error_msg})
