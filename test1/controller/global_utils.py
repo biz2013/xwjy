ae94e6f7ec839636730f2f60f155c15c7c5a9ade
Tao <taozhang@salesforce.com>
2017-12-23 06:26:37 +0700

    adding query to purchase, and fix top menu, add logou

--- test1/controller/global_utils.py
+++ test1/controller/global_utils.py
@@ -1,2 +1,11 @@
 import pytz
 import datetime as dt
+
+from controller.global_constants import *
+
+def user_session_is_valid(request):
+    if (REQ_KEY_USERNAME not in request.session) or (REQ_KEY_USERID not in request.session):
+       return False
+    if (request.session[REQ_KEY_USERNAME] is None) or (request.session[REQ_KEY_USERID] is None):
+       return False
+    return True
--- test1/views/homeview.py
+++ test1/views/homeview.py
@@ -10,6 +10,6 @@ logger = logging.getLogger(__name__)
 
 def home(request):
     """Show the home page."""
-    if REQ_KEY_USERNAME in request.session:
-       return redirect('accountinfo')
-    return render(request, 'html/index.html', {})
+    if (REQ_KEY_USERNAME not in request.session) or (REQ_KEY_USERID not in request.session):
+       return render(request, 'html/index.html', {})
+    return redirect('accountinfo')
