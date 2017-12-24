aebc04f3dbf45bc727058e88570f4089102f0ed9
Tao <taozhang@salesforce.com>
2017-12-24 08:10:50 +0700

    read app_id and key from database

--- test1/views/mypurchaseview.py
+++ test1/views/mypurchaseview.py
@@ -13,13 +13,15 @@ from controller.global_utils import *
 from controller import ordermanager
 
 from users.models import *
+from views.models.orderitem import OrderItem
+from views.models.userpaymentmethodview import *
 from views.models.returnstatus import ReturnStatus
 from views import errorpage
 
 logger = logging.getLogger(__name__)
 
 def show_active_sell_orders(request):
-    #try:
+    try:
        if not user_session_is_valid(request):
           return render(request, 'html/login.html', { 'next_action' : '/purchase/'})
        username = request.session[REQ_KEY_USERNAME]
@@ -30,8 +32,38 @@ def show_active_sell_orders(request):
                 REQ_KEY_USERNAME: username,
                 'previous_call_status' : status})
 
-    #except:
+    except:
        error_msg = 'sell_axfund hit exception: {0}'.format(sys.exc_info()[0])
        logger.error(error_msg)
        return errorpage.show_error(request, ERR_CRITICAL_IRRECOVERABLE,
               '系统遇到问题，请稍后再试。。。{0}'.format(error_msg))
+
+def show_purchase_input(request):
+    if not user_session_is_valid(request):
+       return render(request, 'html/login.html', { 'next_action' : '/purchase/'})
+
+    owner_user_id = request.POST["owner_user_id"]
+    order_id = request.POST["reference_order_id"]
+    owner_login = request.POST["owner_login"]
+    available_units = request.POST["available_units_for_purchase"]
+    print "receive order id %s" % (order_id)
+    print "receive owner_user_id is %s" % (owner_user_id)
+    print "available units for purchase is %s" % (available_units)
+    owner_payment_methods = ordermanager.get_user_payment_methods(int(owner_user_id))
+    #for method in owner_payment_methods:
+    #    print ("provider %s has image %s" % (method.provider.name, method.provider_qrcode_image))
+    sellorder = OrderItem(
+       request.POST["reference_order_id"],
+       owner_user_id,
+       owner_login,
+       request.POST["locked_in_unit_price"],
+       'CYN',
+       0, available_units,
+       '','')
+    print 'sellorder id is here %s' % (sellorder.order_id)
+    login = request.POST['username']
+    return render(request, 'html/input_purchase.html',
+           {'username':'taozhang',
+            'sellorder': sellorder,
+            'owner_payment_methods':owner_payment_methods }
+           )
