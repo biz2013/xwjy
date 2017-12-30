44cc5e5cd6e2dd40a8ad979ca3b5c59af148de50
Tao <taozhang@salesforce.com>
2017-12-30 08:18:17 +0700

    fix syntax for the new changes

---
+++ test1/views/account_cronjob.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+from django.db.models import Q
+from django.shortcuts import render, redirect
+
+# this is for test UI. A fake one
+from config import context_processor
+from controller.global_utils import *
+from users.models import *
+from controller import useraccountinfomanager
+from controller.global_constants import *
+from controller.global_utils import *
+from views.models.returnstatus import ReturnStatus
+from views import errorpage
+
+import logging,json
+
+logger = logging.getLogger(__name__)
+
+def update_account_with_receiving_fund(request):
+    try:
+       client_ip = get_client_ip(request)
+       if client_ip != '127.0.0.1':
+          message = 'update_account_with_receiving_fund() only accept request from localhost. The client ip is {0}'.format(client_ip)
+          loggger.error(message)
+          print message
+          return HttpResponseForbidden()
+       sitesettings = context_processor.settings(request)['settings']
+       useraccountinfomanager.update_account_balance_with_wallet_trx(crypto,
+            wallet_account_name, lookback_count, min_trx_confirmation)
+       return HttpResponse('OK')
+    except:
+       error_msg = 'update_account_with_receiving_fund hit exception: {0}'.format(sys.exc_info()[0])
+       logger.error(error_msg)
+       return HttpResponseServerError(error_msg)
--- test1/views/accountinfoview.py
+++ test1/views/accountinfoview.py
@@ -5,8 +5,7 @@ from django.db.models import Q
 from django.shortcuts import render, redirect
 
 # this is for test UI. A fake one
-from config import context_processor
-from controller.global_utils import *
+from controller.test_model_manager import ModelManager
 from users.models import *
 from controller import useraccountinfomanager
 from controller.global_constants import *
@@ -18,19 +17,19 @@ import logging,json
 
 logger = logging.getLogger(__name__)
 
-def update_account_with_receiving_fund(request):
-    try:
-       client_ip = get_client_ip(request)
-       if client_ip != '127.0.0.1':
-          message = 'update_account_with_receiving_fund() only accept request from localhost. The client ip is {0}'.format(client_ip)
-          loggger.error(message)
-          print message
-          return HttpResponseForbidden()
-       sitesettings = context_processor.settings(request)['settings']
-       useraccountinfomanager.update_account_balance_with_wallet_trx(crypto,
-            wallet_account_name, lookback_count, min_trx_confirmation):
-       return HttpResponse('OK')
-    except:
-       error_msg = 'update_account_with_receiving_fund hit exception: {0}'.format(sys.exc_info()[0])
+def accountinfo(request):
+    #try:
+       if not user_session_is_valid(request):
+          return render(request, 'html/login.html', { 'next_action' : '/accounts/accountinfo/'})
+       username = request.session[REQ_KEY_USERNAME]
+       userId = int(request.session[REQ_KEY_USERID])
+
+       useraccountInfo = useraccountinfomanager.get_user_accountInfo(userId,'AXFund')
+       request.session[REQ_KEY_USERACCOUNTINFO] = useraccountInfo.tojson()
+       return render(request, 'html/myaccount.html', {'useraccountInfo': useraccountInfo,
+                     REQ_KEY_USERNAME: username})
+    #except:
+       error_msg = 'account info hit exception: {0}'.format(sys.exc_info()[0])
        logger.error(error_msg)
-       return HttpResponseServerError(error_msg)
+       return errorpage.show_error(request, ERR_CRITICAL_IRRECOVERABLE,
+              '系统遇到问题，请稍后再试。。。{0}'.format(error_msg))
