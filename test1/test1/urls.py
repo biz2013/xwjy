265e8312e22653fc7a10dc6cd12009c375d905fc
Unknown <alex.chi.wang@gmail.com>
2018-01-08 07:52:41 -0800

    User login, logout, password_reset

--- test1/test1/urls.py
+++ test1/test1/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,13 +23,12 @@ from . import app
 from views import mysellorder, homeview, accountinfoview, mypurchaseview
 from views import account_cronjob, externaladdrview, paymentmethods
 from views import redeemview, heepay_notify_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', homeview.home, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^testpage/$', app.testpage),
-    url(r'^registration/$', app.registration),
-    url(r'^accounts/login/$', app.login, name='login'),
     url(r'^accounts/accountinfo/$', accountinfoview.accountinfo, name='accountinfo'),
     url(r'^accounts/redeem/$', redeemview.redeem),
     url(r'^accounts/external_address/$', externaladdrview.external_address),
@@ -42,5 +41,14 @@ urlpatterns = [
     url(r'^purchase/createorder1/$', mypurchaseview.show_purchase_input, name="input_purchase"),
     url(r'^purchase/createorder2/$', mypurchaseview.create_purchase_order),
     url(r'^account/cron/update_receive/$', account_cronjob.update_account_with_receiving_fund),
-    url(r'^logout/$', app.logout)
+    url(r'^registration/$', app.registration),
+    url(r'^accounts/login/$', auth_views.login, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
+    url(r'^accounts/password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^accounts/password_reset_done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^accounts/password_reset_complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+
+    # url(r'^accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
