0c275152b54690c5757b63b044632e9650f1c5b3
Unknown <alex.chi.wang@gmail.com>
2018-01-14 11:45:29 -0800

    User registration email confirmation

--- test1/test1/app.py
+++ test1/test1/app.py
@@ -7,13 +7,24 @@ from controller.global_utils import *
 
 # this is for test UI. A fake one
 from controller.test_model_manager import ModelManager
+from controller.token import account_activation_token
+
 from controller.global_constants import *
 from controller import loginmanager
 from users.models import *
 from views.models.orderitem import OrderItem
 from views.models.returnstatus import ReturnStatus
+
+from django.template.loader import render_to_string
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.sites.shortcuts import get_current_site
+
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
 
 from test1.forms import *
 import logging,json
@@ -35,38 +46,47 @@ def registration(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            loginmanager.create_login(form, username)            
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
+            # username = form.cleaned_data.get('username')
+            # loginmanager.create_login(form, username)
+            # raw_password = form.cleaned_data.get('password1')
+            # user = authenticate(username=username, password=raw_password)
+            # login(request, user)
+            # return redirect('home')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your blog account.'
+            message = render_to_string('registration/user_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
     else:
         form = SignUpForm()
     return render(request, 'html/register.html', {'form': form})
 
-    # login = UserLogin()
-    # user = User()
-    # user.login = login
-    # if request.method == 'POST':
-    #     login.username = request.POST['username']
-    #     login.password = request.POST['password']
-    #
-    #     rlogger.debug("Get registration request : username %s password %s email %s", login.username, login.password, user.email)
-    #     user.email = request.POST['email']
-    #     print "registration: username %s password %s email %s" % (login.username, login.password, user.email)
-    #     manager = ModelManager()
-    #     rc, msg = manager.register(user)
-    #     if 0 == rc:
-    #         return render(request, 'html/login.html',
-    #           {'message':msg, 'message_type':'success',
-    #           'login': User()})
-    #     return render(request,'html/register.html',
-    #           {'message':msg, 'message_type':'fail', 'registration':user})
-    # else:
-    #     return render(request,'html/register.html',
-    #           {'registration':user})
-
+def activate_user_registration(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('home')
+        #return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
 
 def query_user_open_sell_orders(userlogin):
     return Order.objects.filter(user__login = userlogin).filter(order_type='SELL').filter(~Q(status='FILLED') | ~Q(status='CANCELLED'))
