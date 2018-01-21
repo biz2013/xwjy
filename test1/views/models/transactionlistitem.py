9fdf8bd9c4483155bf7fb2e6caeb37232d48a249
Tao <taozhang@salesforce.com>
2018-01-21 01:29:24 -0800

    add limit and change name

--- test1/views/models/transactionlistitem.py
+++ test1/views/models/transactionlistitem.py
@@ -3,8 +3,8 @@
 class TransactionListItem(object):
     def __init__(self, id, trans_type, balance_update_type, units, balance, locked_balance, available_balance, status , lastupdated_at, crypto):
         lookup = {'CANCEL SELL ORDER':'取消卖单',
-                   'OPEN BUY ORDER': '挂买单',
-                   'OPEN SELL ORDER': '挂卖单',
+                   'OPEN BUY ORDER': '购买',
+                   'OPEN SELL ORDER': '出售',
                    'CANCEL BUY ORDER': '取消买单',
                    'DELIVER ON PURCHASE': '交货',
                    'REDEEM': '提币',
