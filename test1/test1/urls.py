091b524f98e1bc88bfd425a01f50e289e5399a3b
Tao <taozhang@salesforce.com>
2018-01-21 21:00:32 -0800

    fix path

--- test1/test1/urls.py
+++ test1/test1/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     url(r'^axfund/transfer/$', app.transfer),
     url(r'^mysellorder/$', mysellorder.sell_axfund, name="sellorder"),
     url(r'^mysellorder/cancel$', mysellorder.cancel_sell_order, name="cancelsellorder"),
-    url(r'^mysellorder/confirm_payment$', mysellorder.confirm_payment, name="confirm_payment"),
+    url(r'^mysellorder/confirm_payment/$', mysellorder.confirm_payment, name="confirm_payment"),
     url(r'^heepay/confirm_payment/$', heepay_notify_view.heepay_confirm_payment),
     url(r'^purchase/$', mypurchaseview.show_active_sell_orders, name='purchase'),
     url(r'^purchase/createorder1/$', mypurchaseview.show_purchase_input, name="input_purchase"),
