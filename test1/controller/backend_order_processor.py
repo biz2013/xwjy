40d656d5be5bdbe06078227a65f55471a1990976
Tao <taozhang@salesforce.com>
2018-01-23 19:38:27 -0800

    ready to get backend processing

---
+++ test1/controller/backend_order_processor.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import time
+import datetime as dt
+import pytz
+import logging
+from calendar import timegm
+
+from django.db import transaction
+from django.db.models import F, Q, Count
+from django.contrib.auth.models import User
+
+from users.models import *
+from views.models.orderitem import OrderItem
+from views.models.userpaymentmethodview import *
+
+logger = logging.getLogger("site.backend_order_processor")
+
+def cancelPurchaseOrder(orderid, operator):
+    pass
+
+
+def confirmPurchaseOrder(orderid, operator):
+    pass
+
+def getAllUnfilledPurchaseOrder():
+    return Orders.objects.filter(Q(status='PAYING') |\
+       Q(status='PAID' | Q(status='OPEN')).order_by('-lastupdated_at')
