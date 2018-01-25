945a3c56f4bf4b1458a85e9203db4101031bf229
Tao <taozhang@salesforce.com>
2018-01-24 23:31:53 -0800

    adding process for pending order

--- test1/views/order_batch_process_view.py
+++ test1/views/order_batch_process_view.py
@@ -29,18 +29,17 @@ def handle_paying_order(order, order_timeout, appId, appkey):
             logger.error('purchase order {0}: transaction id{1} : no payment bill no yet its status is PAYING'.format(order.order_id, trans.id))
             return;
 
-        heepay = HeePayManager()
-        if trans.payment_status != 'SUCCESS':
-            logger.info('purchase order {0}: transaction id{1} : expired, payment_status: {2}. Query heepay for status...'.format(order.order_id, trans.id, trans.payment_status))
-            payment_status = heepay.get_payment_status(order.order_id,
-                               trans.payment_bill_no, appId, appkey)
-            logger.info('purchase order {0}: transaction id{1} : expired, queried payment_status: {2}. Query heepay '.format(order.order_id, trans.id, payment_status))
-
-        if payment_status in ['PAYSUCCESS','SUCCESS']:
-            ordermanager.confirm_purchase_order(order.order_id, 'admin')
-
         timediff = datetime.datetime().utcnow() - order.lastupdated_at
         if int(timediff.total_seconds()) > order_timeout:
+            heepay = HeePayManager()
+            if trans.payment_status != 'SUCCESS':
+                logger.info('purchase order {0}: transaction id{1} : expired, payment_status: {2}. Query heepay for status...'.format(order.order_id, trans.id, trans.payment_status))
+                json_response = heepay.get_payment_status(order.order_id,
+                                   trans.payment_bill_no, appId, appkey)
+                logger.info('purchase order {0}: transaction id{1} : expired, queried payment_status: {2}. Query heepay '.format(order.order_id, trans.id, payment_status))
+
+            if payment_status in ['PAYSUCCESS','SUCCESS']:
+                ordermanager.update_order_with_heepay_notification(json_response, 'admin')
             if payment_status in ['EXPIREDINVALID','DEVCLOSE','USERABANDON','UNKNOWN']:
                 backend_order_process.cancel_purchase_order(order.order_id,
                   'FAILED', payment_status, 'admin')
@@ -53,23 +52,41 @@ def handle_paying_order(order, order_timeout, appId, appkey):
         error_msg = 'handle_paying_order hit eception {0}'.format(sys.exc_info()[0])
         logger.exception(error_msg)
 
+def handle_paid_order(order, confirmation_timeout):
+    try:
+        timediff = datetime.datetime().utcnow() - order.lastupdated_at
+        if timediff > confirmation_timeout:
+            ordermanager.confirm_purchase_order(order.order_id, 'admin')
+    except Exception as e:
+        error_msg = 'handle_paid_order hit eception {0}'.format(sys.exc_info()[0])
+        logger.exception(error_msg)
 
-
-
+def handle_open_order(order, sell_order_timeout):
+    try:
+        timediff = datetime.datetime().utcnow() - order.lastupdated_at
+        if timediff > confirmation_timeout:
+            ordermanager.cancel_purchase_order(order.order_id,
+              'CANCELLED', 'UNKNOWN', 'admin')
+    except Exception as e:
+        error_msg = 'handle_open_order hit eception {0}'.format(sys.exc_info()[0])
+        logger.exception(error_msg)
 
 def order_batch_process(request):
     try:
         sitesettings = context_processor.settings(request)['settings']
         sell_order_timeout = sitesettings['order_timeout_insec']
         confirmation_timeout = sitesettings['confirmation_timeout_insec']
+        appId = sitesettings['heepay_app_id']
+        appKey = sitesettings['heepay_app_key']
         orders = backend_order_processor.get_unfilled_purchase_orders(request)
         for order in orders:
             if order.status == 'PAYING':
-                handle_paying_order(order, 'admin')
+                handle_paying_order(order.order_id, sell_order_timeout, appId, appKey)
             elif order.status == 'PAID':
-                pass
+                handle_paid_order(order, confirmation_timeout)
             elif order.status == 'OPEN':
-                pass
+                handle_open_order(order, sell_order_timeout)
+
 
 
     except Exception as e:
