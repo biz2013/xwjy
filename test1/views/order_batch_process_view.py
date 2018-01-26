36810baeb24e976c8e5ed5152f92531488d6ddbc
Tao <taozhang@salesforce.com>
2018-01-25 21:40:08 -0800

    fix code

--- test1/views/order_batch_process_view.py
+++ test1/views/order_batch_process_view.py
@@ -27,7 +27,7 @@ logger = logging.getLogger("site.order_batch_process")
 def handle_paying_order(order, order_timeout, appId, appkey):
     try:
         logger.info("handle_paying_order {0}".format(order.order_id))
-        trans = order.get_order_transactions(order.order_id)
+        trans = ordermanager.get_order_transactions(order.order_id)
         if not trans.payment_bill_no:
             logger.error('purchase order {0}: transaction id{1} : no payment bill no yet its status is PAYING'.format(order.order_id, trans.id))
             return;
