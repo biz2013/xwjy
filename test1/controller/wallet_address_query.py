17af41a68045ce5c68bb0fbabf03313a927ed66b
root <root@ip-172-31-11-244.ap-northeast-2.compute.internal>
2018-01-30 10:16:30 +0000

    fix address creation

--- test1/controller/wallet_address_query.py
+++ test1/controller/wallet_address_query.py
@@ -13,11 +13,11 @@ def get_unassigned_userwallets_count():
     return UserWallet.objects.filter(user__isnull=True).count()
 
 def create_user_wallets(addresses, crypto, operator):
-    walletObj = Wallet.object.get(cryptocurrency__currency_code = crypto)
-    operatorObj = User.object.get(username=operator)
+    walletObj = Wallet.objects.get(cryptocurrency__currency_code = crypto)
+    operatorObj = User.objects.get(username=operator)
     with transaction.atomic():
         for addr in addresses:
-            UserWallet.object.create(
+            UserWallet.objects.create(
                wallet = walletObj,
                wallet_addr = addr,
                created_by = operatorObj,
--- test1/views/wallet_address_batch.py
+++ test1/views/wallet_address_batch.py
@@ -6,6 +6,8 @@ import logging,json
 from django.http import HttpResponse, HttpResponseServerError,HttpResponseForbidden
 
 from config import context_processor
+from controller.global_utils import *
+from controller.axfd_utils import *
 from users.models import *
 from controller import axfd_utils
 from controller import wallet_address_query
@@ -21,7 +23,12 @@ def create_wallet_address(request):
             logger.error(message)
             return HttpResponseForbidden()
         sitesettings = context_processor.settings(request)['settings']
-        config_json = json_loads(sitesettings.config_json)
+        axfd_bin_path = sitesettings.axfd_path
+        axfd_datadir = sitesettings.axfd_datadir
+        wallet_account_name = sitesettings.axfd_account_name
+        axfd_tool = AXFundUtility(axfd_bin_path, axfd_datadir,
+                wallet_account_name)
+        config_json = json.loads(sitesettings.config_json)
         batch_size = 20
         if 'wallet_address_batch_size' in config_json:
             batch_size = config_json['wallet_address_batch_size']
@@ -35,12 +42,12 @@ def create_wallet_address(request):
             return
 
         new_addresses = []
-        for i in range(0: batch_size - unassigned_count):
-            new_addresses.append(axfd_utils.create_wallet_address())
+        for i in range(0, batch_size - unassigned_count):
+            new_addresses.append(axfd_tool.create_wallet_address())
             logger.info("Create address[{0}]={1}".format(i, new_addresses[i]))
 
-        wallet_address_query.create_user_wallets(new_addresses, 'AXFund')
-
+        wallet_address_query.create_user_wallets(new_addresses, 'AXFund', 'admin')
+        return HttpResponse('OK')
 
     except Exception as e:
        error_msg = 'create_wallet_address hit exception: {0}'.format(sys.exc_info()[0])
