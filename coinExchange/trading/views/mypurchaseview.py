02afffa212dde27947e86ee3e25e5e0b61f06b64
Ubuntu <ubuntu@ip-172-31-0-229.us-west-2.compute.internal>
2018-02-19 02:50:23 +0000

    fix order processing issue

--- coinExchange/trading/views/mypurchaseview.py
+++ coinExchange/trading/views/mypurchaseview.py
@@ -56,6 +56,9 @@ def show_purchase_input(request):
         if len(useraccountInfo.paymentmethods[0].account_at_provider) == 0:
             messages.error(request, '请先注册支付账号再购买')
             return redirect('accountinfo')
+        if "owner_user_id" not in request.POST:
+            messages.warn("回到主页再进行操作")
+            return redirect('accountinfo')
         owner_user_id = request.POST["owner_user_id"]
         reference_order_id = request.POST["reference_order_id"]
         owner_login = request.POST["owner_login"]
