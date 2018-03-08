e80ff5f08f6fccb65e3bec2bdff135f80388798d
Tao <taozhang@salesforce.com>
2018-03-07 21:45:03 -0800

    add missing file

---
+++ stakingsvc/walletgui/controller/global_constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Value Error Name
+VE_REDEEM_EXCEED_LIMIT='Redeem Exceed Limit'
+VE_ILLEGAL_BALANCE='Illegal Balance'
+
+#
+# These are error level constants tha help us handle
+# errors
+#
+ERR_CRITICAL_RECOVERABLE='ERR_CRITICAL_RECOVERABLE'
+ERR_CRITICAL_IRRECOVERABLE='ERR_CRITICAL_IRRECOVERABLE'
