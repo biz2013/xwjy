1426b4591e5d32153c19958d5a0e1b69eb7d9c62
Tao <taozhang@salesforce.com>
2018-03-31 12:05:21 -0700

    add skeleton files to tradeapi

---
+++ coinExchange/tradeex/controllers/tradex.py
@@ -0,0 +1,15 @@
+class TradeExchangeManager(object):
+    def find_order_to_buy(self):
+        pass
+    
+    def post_sell_order(self):
+        pass
+    
+    def confirm_order(self):
+        pass
+
+    def cancel_order(self):
+        pass
+    
+    def query_order_status(self)
+        pass
