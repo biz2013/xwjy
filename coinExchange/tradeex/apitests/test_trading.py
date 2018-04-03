18b43e66c99150f221308172dfd23c0277431fb3
Tao <taozhang@salesforce.com>
2018-04-03 01:24:16 -0700

    fix test, add response object

--- coinExchange/tradeex/apitests/test_trading.py
+++ coinExchange/tradeex/apitests/test_trading.py
@@ -13,27 +13,33 @@ import json
 class TestPrepurchase(TransactionTestCase):
     fixtures = ['fixture_test_tradeapi.json']
 
+    def setUp(self):
+        pass
+
     def validate_success_prepurchase_response(self, resp_json):
         self.assertEqual(resp_json['return_code'], 'SUCCESS')
         return True
 
-    def test_purchase(self):
-        request = PurchaseAPIRequest('testapi_key', 'test_secret_key',
+    def test_bad_purchase_api_key(self):
+        request = PurchaseAPIRequest('api_key_not_exist', 'secrete_not_exist',
                 '20180320222600_123', # order id
                 0.05, # total fee
                 'heepay', '12738456',
                 '127.0.0.1', #client ip
-                attached='userid:1',
+                attach='userid:1',
                 notify_url='http://testurl',
                 return_url='http://retururl')
         c = Client()
         request_str = request.getPayload()
-        print('send request {0}', request_str)
-        response = c.post('/tradeapi/prepurchase/', request_str,
+        print('send request {0}'.format(request_str))
+        response = c.post('/tradeex/purchasetoken/', request_str,
                           content_type='application/json')
 
-        print('response is ' + json.dumps(response.json()))
+        print('response is {0}'.format(json.dumps(json.loads(response.content), ensure_ascii=False)))
+
+        self.assertEqual(200, response.status_code)
         resp_json = json.loads(response.content)
-        self.assertTrue(self.validate_success_prepurchase_response(resp_json))
+        self.assertEqual(resp_json['return_code'], 'FAIL')
+        self.assertEqual(resp_json['return_msg'], '参数错误')
 
 
