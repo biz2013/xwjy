445aed18a8f3d64aa3f6a0998fe0f025b1e1a931
Tao <taozhang@salesforce.com>
2018-04-06 10:57:25 +0100

    fix tradex in Q query

--- coinExchange/tradeex/apitests/test_trading.py
+++ coinExchange/tradeex/apitests/test_trading.py
@@ -3,10 +3,13 @@
 
 import sys
 sys.path.append('../stakingsvc/')
+from django.contrib.auth.models import User
 from django.test import TestCase, TransactionTestCase
 from django.test import Client
 from tradeapi.data.traderequest import PurchaseAPIRequest
 from tradeex.apitests.tradingutils import *
+from trading.models import *
+from trading.controller import useraccountinfomanager
 import json
 
 # Create your tests here.
@@ -42,16 +45,38 @@ class TestPrepurchase(TransactionTestCase):
         self.assertEqual(resp_json['return_code'], 'FAIL')
         self.assertEqual(resp_json['return_msg'], '参数错误')
         #TODO: show user not found?
-    
+
+    def validate_user_info(self, username):
+        wallet_count = len(UserWallet.objects.all())
+        self.assertTrue(wallet_count==6, "There should be 4 user wallets but have {0}".format(wallet_count))
+        for wallet in UserWallet.objects.all():
+            print('wallet {0}, user {1} {2}, balance {3}, address {4}, coin {5}'.format(
+                wallet.id, wallet.user.id, wallet.user.username, wallet.balance, wallet.wallet_addr,
+                wallet.wallet.cryptocurrency.currency_code
+            ))
+
+        useraccountInfo = useraccountinfomanager.get_user_accountInfo(User.objects.get(username=username),'AXFund')
+        self.assertTrue(useraccountInfo.balance > 0, "the balance of {0} should be larger than 0".format(username))
+        self.assertTrue(useraccountInfo.available_balance > 0, "the available balance of {0} should be larger than 0".format(username))
+        self.assertTrue(useraccountInfo.paymentmethods, "user {0} should have payment info".format(username))
+        self.assertEqual(1, len(useraccountInfo.paymentmethods), "There should be 1 payment method for user {0}".format(username))
+        self.assertEqual('heepay', useraccountInfo.paymentmethods[0].provider_code, "user {0}\'s payment method should come from heepay".format(username))
+        self.assertTrue(useraccountInfo.paymentmethods[0].account_at_provider, "User {0} should have account at heepay".format(username))
+
     def create_no_fitting_order(self):
         print('create_no_fitting_order()')
+        self.validate_user_info('tttzhang2000@yahoo.com')
         resp = create_axfund_sell_order('tttzhang2000@yahoo.com', 'user@123', 100, 0.5, 'CNY')
         self.assertEqual(200, resp.status_code, "Create order of 100 units should return 200")
-        self.assertTrue('系统遇到问题'.encode('utf-8') in resp.content, 'Create order of 100 units hit issue')
+        self.assertFalse('系统遇到问题'.encode('utf-8') in resp.content,'Create order of 100 units hit issue')
+
+        self.validate_user_info('yingzhou61@yahoo.ca')
         resp = create_axfund_sell_order('yingzhou61@yahoo.ca', 'user@123', 200, 0.3, 'CNY')
         self.assertEqual(200, resp.status_code, "Create order of 200 units should return 200")
-        self.assertTrue('系统遇到问题'.encode('utf-8') in resp.content, 'Create order of 200 units hit issue')
+        self.assertFalse('系统遇到问题'.encode('utf-8') in resp.content, 'Create order of 200 units hit issue')
 
+        for order in Order.objects.all():
+            print('order {0} order_type {1} sub_type {2}'.format(order.order_id, order.order_type, order.sub_type))
 
     def test_purchase_no_fitting_order(self):
         self.create_no_fitting_order()
