48e7baab7bc3348f48d94a09587d65d166889ca9
Tao <taozhang@salesforce.com>
2018-04-14 23:15:04 +0200

    fix test

--- coinExchange/tradeex/responses/heepayresponse.py
+++ coinExchange/tradeex/responses/heepayresponse.py
@@ -5,11 +5,16 @@ import hashlib
 
 class HeepayResponse(object):
 
+    def __init__(self, api_secret):
+        self.api_secret = api_secret
+
     def __sign(self, json):
         sorted_keys = sorted(json.keys())
         count = 0
         str_to_be_signed = ""
         for key in sorted_keys:
+            if key.startswith('return'):
+                continue
             if count > 0:
                 str_to_be_signed = str_to_be_signed + '&'
             str_to_be_signed = '{0}{1}={2}'.format(str_to_be_signed, key, json[key])
@@ -18,34 +23,42 @@ class HeepayResponse(object):
         m = hashlib.md5()
         m.update(str_to_be_signed.encode('utf-8'))
         return m.hexdigest().upper()
-    
-    def parseFromJson(self, json_data, api_secret):
-        self.api_secret = api_secret
+
+    @classmethod    
+    def parseFromJson(cls, json_data, api_secret):
+        resp = HeepayResponse(api_secret)
+
         if 'return_code' not in json_data:
             raise ValueError('Invalid heepay response: missing return_code')
         if 'return_msg' not in json_data:
             raise ValueError('Invalid heepay response: missing return_msg')
         if 'sign' not in json_data:
             raise ValueError('Invalid heepay response: missing sign data')
-        if 'result_code' not in json_data:
-            raise ValueError('Invalid heepay response: missing result_code')
-        if 'result_msg' not in json_data:
-            raise ValueError('Invalid heepay response: missing result_msg')
+        if json_data['return_code'] == 'SUCCESS':
+            if 'result_code' not in json_data:
+                raise ValueError('Invalid heepay response: missing result_code')
+            if 'result_msg' not in json_data:
+                raise ValueError('Invalid heepay response: missing result_msg')
+        else:
+            if 'result_code' in json_data:
+                raise ValueError('Invalid heepay response: result_code field should not exist if return_code=FAIL')
+            if 'result_msg' in json_data:
+                raise ValueError('Invalid heepay response: result_msg field should not exist if return_code=FAIL')
 
-        calculated_sign = self.__sign(json_data)
+        calculated_sign = resp.__sign(json_data)
         if calculated_sign != json_data['sign']:
             raise ValueError('Invalid heepay response: signature does not match.  Expected {0} but got {1}'.format(
                 json_data['sign'], calculated_sign
             ))
 
-        self.api_key = json_data['app_id']
-        self.subject = json_data['subject']
-        self.attach = json_data['attach']
-        self.total_fee = json_data['total_fee']
-        self.out_trade_no = json_data['out_trade_no']
-        self.hy_url = json_data['hy_url']
-        self.hy_pay_id = json_data['hy_pay_id']
-        self.sign = calculated_sign
+        resp.api_key = json_data['app_id']
+        resp.subject = json_data['subject']
+        resp.attach = json_data['attach']
+        resp.total_fee = json_data['total_fee']
+        resp.out_trade_no = json_data['out_trade_no']
+        resp.hy_url = json_data['hy_url']
+        resp.hy_pay_id = json_data['hy_pay_id']
+        resp.sign = calculated_sign
 
 
         
