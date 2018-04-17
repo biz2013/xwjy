8d54eceed25ef8334b415c2b90fdce912a9ba0f8
Tao <taozhang@salesforce.com>
2018-04-16 19:36:50 -0700

    adding notification

--- coinExchange/tradeex/utils.py
+++ coinExchange/tradeex/utils.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+from tradeapi.data.purchaseapiresponse import PurchaseAPIResponse
+
+def create_return_msg_from_valueError(valueError):
+    msg_map= {
+        'PAYMENT_ACCOUNT_NOT_FOUND':'数据错误:通知系统服务',
+        'TOO_MANY_ACCOUNTS_AT_PROVIDER': '数据错误:通知系统服务',
+        'NOT_SELL_ORDER_FOUND':'数据错误:通知系统服务',
+        'NOT_SELL_ORDER_CAN_BE_LOCKED':'数据错误:通知系统服务'
+    }
+    return msg_map[valueError] if valueError in msg_map else '系统错误:通知系统服务'
+
+def create_result_msg_from_valueError(valueError):
+    msg_map= {
+        'PAYMENT_ACCOUNT_NOT_FOUND':'数据错误:通知系统服务',
+        'TOO_MANY_ACCOUNTS_AT_PROVIDER': '数据错误:通知系统服务',
+        'NOT_SELL_ORDER_FOUND':'数据错误:通知系统服务',
+        'NOT_SELL_ORDER_CAN_BE_LOCKED':'数据错误:通知系统服务'
+    }
+    return msg_map[valueError] if valueError in msg_map else '系统错误:通知系统服务'
+
