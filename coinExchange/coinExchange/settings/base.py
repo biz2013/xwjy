44a650fc1058cf01f9e7638ea5591aa403b89a34
Ubuntu <ubuntu@ip-172-31-8-165.us-west-2.compute.internal>
2018-04-25 02:05:41 +0000

    change email password and fix the site

--- coinExchange/coinExchange/settings/base.py
+++ coinExchange/coinExchange/settings/base.py
@@ -152,7 +152,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'axfundnoreply@gmail.com'
-EMAIL_HOST_PASSWORD = 'axTrade@123'
+EMAIL_HOST_PASSWORD = 'AXFund@017'
 
 LOGGING = {
     'version': 1,
