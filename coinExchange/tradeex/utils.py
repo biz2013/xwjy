3b71354c3243dc392d799ea7c4606e6ac048f4c6
Tao <taozhang@salesforce.com>
2018-05-26 23:27:21 -0700

    fix all test in apitests

--- coinExchange/tradeex/utils.py
+++ coinExchange/tradeex/utils.py
@@ -23,7 +23,8 @@ def create_return_msg_from_valueError(valueError):
         'PAYMENT_ACCOUNT_NOT_FOUND':'数据错误:通知系统服务',
         'TOO_MANY_ACCOUNTS_AT_PROVIDER': '数据错误:通知系统服务',
         'NOT_SELL_ORDER_FOUND':'数据错误:通知系统服务',
-        'NOT_SELL_ORDER_CAN_BE_LOCKED':'数据错误:通知系统服务'
+        'NOT_SELL_ORDER_CAN_BE_LOCKED':'数据错误:通知系统服务',
+        'USER_NOT_FOUND_WITH_API_KEY':'未找到您的账户:通知系统服务'
     }
     return msg_map[valueError] if valueError in msg_map else '系统错误:通知系统服务'
 
