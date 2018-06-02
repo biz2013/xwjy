0fdeb8752524573e3a6c8cb0053fa857322d300a
Tao <taozhang@salesforce.com>
2018-06-02 02:59:28 -0700

    fix log

--- coinExchange/tradeex/utils.py
+++ coinExchange/tradeex/utils.py
@@ -7,12 +7,13 @@ import logging
 logger = logging.getLogger("tradeex.utils")
 
 def sign_api_content(json, secret_key):
-    logger.info("sign_api_content()")
+    logger.info("sign_api_content({0})").format(json))
     sorted_keys = sorted(json.keys())
     str_to_be_signed = ""
     for key in sorted_keys:
         str_to_be_signed = '{0}{1}={2}&'.format(str_to_be_signed, key, json[key])
     str_to_be_signed = '{0}key={1}'.format(str_to_be_signed, secret_key)
+    logger.info("str_to_be_signed={0}".format(str_to_be_signed))
     m = hashlib.md5()
     logger.debug("sign_api_content(): str to be signed {0}".format(str_to_be_signed))
     m.update(str_to_be_signed.encode('utf-8'))
