a146721aabfc647988f7692fa248fb9b525eb655
Tao <taozhang@salesforce.com>
2018-06-02 02:37:31 -0700

    fix signature

--- coinExchange/tradeex/utils.py
+++ coinExchange/tradeex/utils.py
@@ -11,7 +11,7 @@ def sign_api_content(json, secret_key):
     str_to_be_signed = ""
     for key in sorted_keys:
         str_to_be_signed = '{0}{1}={2}&'.format(str_to_be_signed, key, json[key])
-    str_to_be_signed = '{0}key={1}'.format(str_to_be_signed, secret_key.upper())
+    str_to_be_signed = '{0}key={1}'.format(str_to_be_signed, secret_key)
     m = hashlib.md5()
     logger.debug("sign_api_content(): str to be signed {0}".format(str_to_be_signed))
     m.update(str_to_be_signed.encode('utf-8'))
