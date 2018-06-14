8f06b7a6dbaef772832c505c980d9e8968eb5337
Tao <taozhang@salesforce.com>
2018-06-13 21:55:45 -0700

    fix dev setting

--- coinExchange/coinExchange/settings/dev.py
+++ coinExchange/coinExchange/settings/dev.py
@@ -3,3 +3,9 @@ from .base import *
 ALLOWED_HOSTS = ['54.218.104.21', 'localhost', '127.0.0.1']
 # send email to console
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+STATIC_ROOT = "/var/www/coinexchange/static/"
+
+MEDIA_ROOT = "/var/www/coinexchange/media/"
+
+ALLOWED_HOSTS = ['54.203.195.52', 'localhost', '127.0.0.1', '[::1]']
