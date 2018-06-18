68f725eae5475ecf450372a79247206c0b0077d0
root <root@ip-172-31-0-229.us-west-2.compute.internal>
2018-06-18 19:43:03 +0000

    add test script tools

---
+++ coinExchange/admin/samples/create_test_user.sh
@@ -0,0 +1,2 @@
+echo "curl -L -v -H content-Type=applications/json http://localhost/setup_create_api_user/ --data @$1"
+curl -L -v -H content-Type=applications/json http://localhost/setup_create_api_user/ --data @$1
