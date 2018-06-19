51ee96ca0b36be81a431872a24017d14bac172a5
root <root@ip-172-31-0-229.us-west-2.compute.internal>
2018-06-19 08:46:53 +0000

    add backup script

--- coinExchange/admin/backup.sh
+++ coinExchange/admin/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "we have $# args"
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
    echo "usage: backup  datadumpfile db_schemabackup setting_style"
    echo " backupdatafile: full path of the dumped data file"
    echo " setting_style: test, product, dev?"
@@ -9,7 +9,8 @@ if [ $# -ne 2 ]; then
 fi
 
 BACKUPDATAFILE=$1
-SETTING_STYLE=$2
+SCHEMABACKUPFILE=$2
+SETTING_STYLE=$3
 
 WORKHOME=/home/ubuntu/workspace/xwjy/coinExchange
 echo "cd $WORKHOME"
@@ -20,33 +21,11 @@ echo "source dj2env/bin/activate"
 source dj2env/bin/activate
 
 echo "dump database data"
-echo "python manage.py dumpdata auth.user trading tradeex $BACKUPDATAFILE --settings=coinExchange.settings.$SETTING_STYLE"
-python manage.py dumpdata auth.user trading tradeex $BACKUPDATAFILE --settings=coinExchange.settings.$SETTING_STYLE
+echo "python manage.py dumpdata auth.user trading tradeex | python -mjson.tool >  $BACKUPDATAFILE"
+python manage.py dumpdata auth.user trading tradeex | python -mjson.tool > $BACKUPDATAFILE
 
 echo "backup migration data"
-tar cvxf $WORKHOME/migration
-
-echo "delete logs"
-rm -rf $WORKHOME/logs/site.log*
-
-echo "start service using $STARTING_STYLE"
-if [ "$STARTING_STYLE" = "apache" ]; then
-   sudo apachectl start 
-elif [ "$STARTING_STYLE" = "systemctl" ]; then
-   sudo /bin/systemctl start axfund-django.service
-else
-   echo "Unknown service control style $STARTING_STYLE"
-   exit -1
-fi
-
-echo "sleep 5 seconds"
-sleep 5
-
-echo "setup test"
-/usr/bin/curl -L -v http://localhost/setuptest/
+echo "/bin/tar cvzf $WORKHOME/$SCHEMABACKUPFILE tradeex/migrations trading/migrations"
+/bin/tar cvzf $WORKHOME/$SCHEMABACKUPFILE tradeex/migrations trading/migrations
 
 echo "Done."
- 
-
-
-
