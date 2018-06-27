e8a43a2b7636827d8e2374779d6ff04676fac657
Tao <taozhang@salesforce.com>
2018-06-26 23:16:10 -0700

    add missing file

--- stakingsvc/walletgui/models.py
+++ stakingsvc/walletgui/models.py
@@ -148,7 +148,7 @@ class UserWalletTransaction(models.Model):
    lastupdated_by = models.ForeignKey(User, related_name='UserWallet_trans_lastupdated_by', on_delete=models.SET_NULL, null=True)
 
 class UserExternalWalletAddress(models.Model):
-   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   user = models.ForeignKey('User', on_delete=models.CASCADE)
    cryptocurrency = models.ForeignKey('Cryptocurrency', on_delete=models.CASCADE)
    address = models.CharField(max_length=128)
    alias = models.CharField(max_length=32, null=True)
@@ -225,3 +225,82 @@ class UserProfile(models.Model):
    lastupdated_at = models.DateTimeField(auto_now=True)
    lastupdated_by = models.ForeignKey(User, related_name='UserProfile_lastupdated_by', on_delete=models.SET_NULL, null=True)
     
+class APIUserAccount(models.Model):
+    ACCOUNT_STATUS =(('OPEN', 'Open'),
+        ('ACTIVE', 'Active'),
+        ('PENDING', 'Pending'),
+        ('SUSPENDED', 'Suspended'),
+        ('CLOSED', 'Closed'))
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    accountNo = models.CharField(max_length=32, unique=True)
+    apiKey = models.CharField(max_length=128, primary_key=True)
+    secretKey = models.CharField(max_length=128, unique=True)
+    status = models.CharField(max_length=32, choices=ACCOUNT_STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='APIUserAccount_created_by', on_delete=models.SET_NULL, null=True)
+    lastupdated_at = models.DateTimeField(auto_now=True)
+    lastupdated_by = models.ForeignKey(User, related_name='APIUserAccount_lastupdated_by', on_delete=models.SET_NULL, null=True)
+
+class APIUserExternalWalletAddress(models.Model):
+    api_account = models.OneToOneField(APIUserAccount, on_delete=models.CASCADE)
+    cryptocurrency = models.ForeignKey('trading.Cryptocurrency', on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
+    alias = models.CharField(max_length=32, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='APIUserExternalWalletAddress_created_by', on_delete=models.SET_NULL, null=True)
+    lastupdated_at = models.DateTimeField(auto_now=True)
+    lastupdated_by = models.ForeignKey(User, related_name='APIUserExternalWalletAddress_lastupdated_by', on_delete=models.SET_NULL, null=True)
+
+class APIUserTransaction(models.Model):
+    PAYMENT_STATUS=(
+        ('UNKNOWN', 'Unknown'),
+        ('NOTSTARTED', 'Not Started'),
+        ('STARTING', 'Starting'),
+        ('PAYSUCCESS','PaySuccess'),
+        ('SUCCESS','Success'),
+        ('EXPIREDINVALID','ExpiredInvalid'),
+        ('USERABANDON', 'UserAbandon'),
+        ('DEVCLOSE', 'DevClose'),
+        ('FAILURE', 'Failure')
+    )
+    TRADE_STATUS=(
+        ('UNKNOWN', 'Unknown'),
+        ('NOTSTARTED', 'Not Started'),
+        ('INPROGRESS', 'InProgress'),
+        ('PAIDSUCCESS','PaidSuccess'),
+        ('SUCCESS','Success'),
+        ('PAYFAILED', 'Pay Failed'),
+        ('SYSTEMERROR', 'SystemError'),
+        ('USERCANCELLED', 'UserCancelled'),
+        ('EXPIRED', 'Expired')
+    )
+    transactionId = models.CharField(max_length=128, primary_key=True)
+    api_out_trade_no = models.CharField(max_length=128, default='')
+    api_user = models.ForeignKey(APIUserAccount, on_delete=models.CASCADE)
+    payment_provider = models.ForeignKey(PaymentProvider, on_delete=models.CASCADE)
+    reference_order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    reference_bill_no = models.CharField(max_length=64, null=True)
+    payment_account = models.CharField(max_length=32, null=True)
+    action = models.CharField(max_length=32)
+    client_ip = models.CharField(max_length=20, null=True)
+    subject = models.CharField(max_length=256, default='')
+    total_fee = models.IntegerField(default=0)
+    real_fee = models.IntegerField(default=0)
+    attach = models.CharField(max_length=1024, null=True)
+    request_timestamp = models.CharField(max_length=32, null=True)
+    original_request = models.CharField(max_length=2048)
+    payment_provider_last_notify = models.CharField(max_length=4096, null=True, default=None)
+    payment_provider_last_notified_at = models.DateTimeField(auto_now_add=False, null=True)
+    expire_in_sec = models.IntegerField(default=600)
+    payment_status = models.CharField(max_length=32, choices=PAYMENT_STATUS, default='UNKNOWN')
+    trade_status = models.CharField(max_length=32, choices=TRADE_STATUS, default='UNKNOWN')
+    notify_url = models.CharField(max_length=1024, null=True)
+    return_url = models.CharField(max_length=1024, null=True)
+    last_notify = models.CharField(max_length=2048, null=True)
+    last_notify_response = models.CharField(max_length=16, null=True)
+    last_notified_at = models.DateTimeField(auto_now_add=False, null=True)
+    last_status_description = models.CharField(max_length=1024, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='APIUserTransaction_created_by', on_delete=models.SET_NULL, null=True)
+    lastupdated_at = models.DateTimeField(auto_now=True)
+    lastupdated_by = models.ForeignKey(User, related_name='APIUserTransaction_lastupdated_by', on_delete=models.SET_NULL, null=True)        
--- stakingsvc/walletgui/views/purchaseview.py
+++ stakingsvc/walletgui/views/purchaseview.py
@@ -1,24 +1,112 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys
+import os, sys, math, json
+import qrcode
+import datetime as dt
+import pytz
+
+from django.conf import settings
+from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import render, redirect
 
 # this is for test UI. A fake one
+from tradeapi.apiclient import APIClient
+from tradeapi.data.tradeapirequest import TradeAPIRequest
+from tradeapi.data.api_const import *
 from walletgui.views import errorpageview
 from walletgui.controller.global_constants import *
 from walletgui.views.models.useraccountinfo import *
 from walletgui.views.models.userpaymentmethod import *
+from walletgui.models import *
 #from django.contrib.auth.decorators import login_required
 
 import logging,json
 
 logger = logging.getLogger("site.balance")
 
-#@login_required
-def show(request):
+def create_api_request(method, api_user, amount, payment_provider_code, payment_account_str):
+    frmt_date = dt.datetime.now(pytz.timezone('Asia/Taipei')).strftime("%Y%m%d%H%M%_S")
+    total_fee = int(amount*100)
+    out_trade_no = 'API_TRDNO_{0}'.format(frmt_date)
+    return TradeAPIRequest(
+        API_METHOD_PURCHASE,
+        api_user.apikey, api_user.secretKey,
+        out_trade_no,
+        total_fee=total_fee,
+        expire_minute=settings.TRADE_API_CALL_TIMEOUT_IN_MINUTES,
+        payment_provider=payment_provider_code,
+        payment_account=payment_account_str,
+        client_ip='127.0.0.1', #client ip
+        subject='充值请求')
+
+
+def create_qrcode_image(content, qrcode_filename, base_dir):
+    dst = os.path.join(base_dir, 'qrcode', qrcode_filename)
+    logger.info('generate qrcode for {0} into {1}'.format(content, dst))
+    myQR = qrcode.QRCode(
+            version = 1,
+            error_correction = qrcode.constants.ERROR_CORRECT_H,
+            box_size = 6,
+            border = 2,
+            )
+    myQR.add_data(content)
+    img = myQR.make_image()
+    img.save(dst)
+    img_path = os.path.join('qrcode', qrcode_filename)
+    return img_path
+
+@login_required
+def purchase(request):
     try:
-       return render(request, 'walletgui/purchase_investment.html')
+        if request.method == 'POST':
+            amount = float(request.POST["amount"]) if request.POST["amount"] else 0
+            payment_provider = request.POST['payment_provider']
+            if math.fabs(amount) < 0.00000001:
+                messages.error(request, '充值数量不可为零')
+                return render(request, 'walletgui/purchase/', { 'amount': amount, 'payment_provider': payment_provider})
+            if not payment_provider:
+                msessages.error(request, '请选择支付提供商')
+                return render(request, 'walletgui/purchase/', { 'amount': amount, 'payment_provider': payment_provider})
+            try:
+                api_user =  APIUserAccount.objects.get(apikey=settings.MY_APIKEY)
+            except APIUserAccount.DoesNotExist:
+                raise ValueError(ERR_USER_NOT_FOUND_BASED_ON_APPID)
+            except APIUserAccount.MultipleObjectsReturned:
+                raise ValueError(ERR_MORE_THAN_ONE_USER_BASED_ON_APPID)
+
+            try:
+                paymentmethod = UserPaymentMethod.objects.get(
+                    user__id = api_user.user.id, 
+                    provider__code = payment_provider)
+            except UserPaymentMethod.DoesNotExist:
+                raise ValueError(ERR_PAYMENTMETHOD_NOT_FOUND)
+            except UserPaymentMethod.MultipleObjectsReturned:
+                raise ValueError(ERR_MORE_THAN_ONE_PAYMENTMETHOD_FOUND)
+
+            request_obj = create_api_request(
+                API_METHOD_PURCHASE,
+                api_user, 
+                amount,paymentmethod.provider.code,
+                paymentmethod.account_at_provider)
+
+            url = TRADE_API_PURCHASE_URL_TEMPLATE.format(settings.TRADE_API_HOST)
+            api_client = APIClient(url)
+            resp_json = api_client.send_json_request(request_obj.getPayload())
+            if resp_json["return_code"] != 'SUCCESS':
+                logger.error('purchase(): get failure api response: {0}'.format(json.dumps(resp_json, ensure_ascii=False)))
+                errmsg = '充值请求遇到问题：{0}'.format(esp_json["return_code"])
+                if 'result_msg' in resp_json:
+                    errmsg = '{0}-{1}'.format(errmsg, resp_json['result_msg'])
+                messages.error(errmsg)
+            else:
+                qrcode_filename = '{0}_{1}.png'.format(
+                    resp_json['out_trade_no'], resp_json['trx_bill_no']
+                )
+                qrcode_img_url_path = create_qrcode_image(
+                    resp_json['payment_url'], qrcode_filename, settings.MEDIA_ROOT)
+                
+                return render(request, 'purchase_qrcode.html', {'qrcode_file': qrcode_img_url_path})
     except Exception as e:
        error_msg = '用户主页显示遇到错误: {0}'.format(sys.exc_info()[0])
        logger.exception(error_msg)
