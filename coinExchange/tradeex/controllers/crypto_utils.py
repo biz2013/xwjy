c39cbbc3157c7eef671cb674c10cfa85d77d1362
Tao <taozhang@salesforce.com>
2018-07-07 17:08:56 -0700

    fix comment issue of sending fund

--- coinExchange/tradeex/controllers/crypto_utils.py
+++ coinExchange/tradeex/controllers/crypto_utils.py
@@ -38,7 +38,7 @@ class CryptoUtility(object):
         ))
         result_str = subprocess.check_output(
            [self.cnyd_path, '-datadir=%s'%(self.cnyd_datadir), 'sendtoaddress',
-            dst, str(amount), '\"{0}\"'.format(comment)])
+            dst, str(amount), '{0}'.format(comment)])
         result_str = result_str.decode('utf-8')
         logger.info("send to address return transaction id {0}".format(
             result_str))
