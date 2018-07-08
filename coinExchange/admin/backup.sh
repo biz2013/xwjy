3029abef61122fe82ba1e61355223135b531aaf1
biz2013 <ttthang2000@yahoo.com>
2018-07-08 04:38:49 +0000

    adding backup command

--- coinExchange/admin/backup.sh
+++ coinExchange/admin/backup.sh
@@ -1,31 +1,62 @@
 #!/bin/bash
-echo "we have $# args"
 
-if [ $# -ne 3 ]; then
-   echo "usage: backup  datadumpfile db_schemabackup setting_style"
-   echo " backupdatafile: full path of the dumped data file"
-   echo " setting_style: test, product, dev?"
-   exit -1
+usage(){
+  echo "usage: backup setting_style [label]"
+  echo "  setting_style: production | dev"
+  echo "  label: the suffix for the backfiles, if omitted, system will use yyyy_mm_dd_HH_MM_SS"
+  exit 1
+}
+
+
+LABEL=$(date '+%Y_%m_%d_%H_%M_%S')
+SETTING=production
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+  usage
 fi
 
-BACKUPDATAFILE=$1
-SCHEMABACKUPFILE=$2
-SETTING_STYLE=$3
+if [ $# -le 1 ]; then
+  SETTING=$1
+fi
+
+if [ $# -eq 2 ]; then
+  LABEL=$2
+fi
+
+
+BACKUPDATAFILE=datadump_$LABEL.json
+SCHEMABACKUPFILE=schema_$LABEL.tgz
+LOGBACKUPFILE=logs_$LABEL.tgz
+
+echo "will create data backup $BACKUPDATAFILE"
+echo "will create schema backup $SCHEMABACKUPFILE"
+echo "will create log backup $LOGBACKUPFILE"
+echo "backup will use $SETTING config"
 
 WORKHOME=/home/ubuntu/workspace/xwjy/coinExchange
+BACKUPDIR=$WORKHOME/site-backup
+
 echo "cd $WORKHOME"
 cd $WORKHOME
 
+if [ ! -d "$BACKUPDIR" ]; then
+  echo "create site back dir  $BACKUPDIR"
+  mkdir $BACKUPDIR
+fi
 
 echo "source dj2env/bin/activate"
 source dj2env/bin/activate
 
 echo "dump database data"
-echo "python manage.py dumpdata auth.user trading tradeex | python -mjson.tool >  $BACKUPDATAFILE"
-python manage.py dumpdata auth.user trading tradeex | python -mjson.tool > $BACKUPDATAFILE
+echo "python manage.py dumpdata auth.user trading tradeex --settings=coinExchange.settings.$SETTING | python -mjson.tool > $BACKUPDIR/$BACKUPDATAFILE"
+python manage.py dumpdata auth.user trading tradeex --settings=coinExchange.settings.$SETTING | python -mjson.tool > $BACKUPDIR/$BACKUPDATAFILE
 
-echo "backup migration data"
-echo "/bin/tar cvzf $WORKHOME/$SCHEMABACKUPFILE tradeex/migrations trading/migrations"
-/bin/tar cvzf $WORKHOME/$SCHEMABACKUPFILE tradeex/migrations trading/migrations
+echo "backup db schema data"
+echo "/bin/tar cvzf $BACKUPDIR/$SCHEMABACKUPFILE tradeex/migrations trading/migrations"
+/bin/tar cvzf $BACKUPDIR/$SCHEMABACKUPFILE tradeex/migrations trading/migrations
+
+echo "backup log files"
+echo "/bin/tar cvzf $BACKUPDIR/$LOGBACKUPFILE logs"
+/bin/tar cvzf $BACKUPDIR/$LOGBACKUPFILE logs
 
 echo "Done."
+exit 0
