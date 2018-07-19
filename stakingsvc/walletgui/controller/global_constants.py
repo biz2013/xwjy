19f665d70a9bced937b7d61c77179053ec5a4261
Tao <taozhang@salesforce.com>
2018-07-18 17:14:19 -0700

    fix dashboard

--- stakingsvc/walletgui/controller/global_constants.py
+++ stakingsvc/walletgui/controller/global_constants.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#
+# This define all the field keys for HTTP requests/session
+#
+REQ_KEY_USERNAME = 'username'
+REQ_KEY_USERID = 'userid'
+REQ_KEY_USERACCOUNTINFO = 'useraccountinfo'
+
 # Value Error Name
 VE_REDEEM_EXCEED_LIMIT='Redeem Exceed Limit'
 VE_ILLEGAL_BALANCE='Illegal Balance'
@@ -11,3 +18,14 @@ VE_ILLEGAL_BALANCE='Illegal Balance'
 #
 ERR_CRITICAL_RECOVERABLE='ERR_CRITICAL_RECOVERABLE'
 ERR_CRITICAL_IRRECOVERABLE='ERR_CRITICAL_IRRECOVERABLE'
+
+# heepay url
+HEEPAYREGISTRY='https://wallet.heepay.com/Account/Register?returnUrl=%s'
+
+
+PAYMENT_NORMAL_STATUS = {'Not Started': 'NOTSTARTED','PaySuccess':'PAYSUCCESS',
+         'Starting':'STARTING', 'Unknown':'UNKNOWN'
+         }
+PAYMENT_BAD_STATUS = { 'ExpiredInvalid': 'EXPIREDINVALID',
+         'UserAbandon':'USERABANDON', 'DevClose':'DEVCLOSE',
+         'Failure':'FAILURE'}
--- stakingsvc/walletgui/views/dashboard.py
+++ stakingsvc/walletgui/views/dashboard.py
@@ -3,22 +3,25 @@
 
 import logging
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 from walletgui.controller.crypto_utils import CryptoUtility
 from walletgui.controller.walletmanager import WalletManager
+from walletgui.views import errorpageview
 
 logger = logging.getLogger("site.dashboard")
 
 @login_required
 def show(request):
-    crypto_util = WalletManager.create_fund_util('CNY')
-    wallet = WalletMAnager.get_wallet_balance(crypto_util, request.user.username, 'CNY')
-    userpaymentmethod = PaymentMethodManager.get_payment_method(request.user.username)
-    useraccountInfo = UserAccountInfo(request.user.id,
-        wallet.balance, wallet.locked_balance, wallet.available_balance,
-        wallet.wallet_addr, None, [ userpaymentmethod ])
-    return render(request, 'walletgui/balance.html',
-        {'account': useraccountInfo })
+    try:
+        crypto_util = WalletManager.create_fund_util('CNY')
+        wallet = WalletMAnager.get_wallet_balance(crypto_util, request.user.username, 'CNY')
+        userpaymentmethod = PaymentMethodManager.get_payment_method(request.user.username)
+        useraccountInfo = UserAccountInfo(request.user.id,
+            wallet.balance, wallet.locked_balance, wallet.available_balance,
+            wallet.wallet_addr, None, [ userpaymentmethod ])
+        return render(request, 'walletgui/balance.html',
+            {'account': useraccountInfo })
     except Exception as e:
         error_msg = '用户主页显示遇到错误: {0}'.format(sys.exc_info()[0])
         logger.exception(error_msg)
