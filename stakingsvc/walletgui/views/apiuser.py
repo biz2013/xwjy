ce40060e5f2786d9d36f27bccecf051127e808cc
Tao <taozhang@salesforce.com>
2018-07-21 21:02:11 -0700

    fix the code

--- stakingsvc/walletgui/views/apiuser.py
+++ stakingsvc/walletgui/views/apiuser.py
@@ -4,7 +4,7 @@ import sys
 
 from django.views.decorators.csrf import csrf_exempt
 from django.auth import User
-from walletgui.controller apiusermanager import APIUserManager
+from walletgui.controller.apiusermanager import APIUserManager
 
 import logging,json
 
