a1b23b590f8dad9516f2494374c0d4936e3a6376
Tao <taozhang@salesforce.com>
2018-08-02 23:08:26 -0700

    fix heepay call

--- coinExchange/trading/controller/heepaymanager.py
+++ coinExchange/trading/controller/heepaymanager.py
@@ -20,6 +20,7 @@ class HeePayManager(object):
    def create_heepay_payload(self, wallet_action, order_id_str, app_id, app_key,
          client_ip, amount, seller_account, buyer_account, notify_url,
          return_url):
+       logger.info('create_heepay_payload(amount:{0}'.format(amount))
        jsonobj = {}
        jsonobj['method'] = wallet_action
        jsonobj['version'] = '1.0'
@@ -34,7 +35,7 @@ class HeePayManager(object):
        amount_in_dollar = round(amount, 2)
        amount_str = str(int(amount_in_dollar)*100)
        biz_content = biz_content + ('\"subject\":\"购买{0}元\",'.format(amount_str))
-       biz_content = biz_content + ('\"total_fee\":\"{0}\",'.format(amount_str))
+       biz_content = biz_content + ('\"total_fee\":\"{0}\",'.format(amount_in_dollar))
        biz_content = biz_content + ('\"api_account_mode\":\"Account\",')
        #biz_content = biz_content + ('\"from_account\":\"{0}\",'.format(buyer_account))
        biz_content = biz_content + ('\"to_account\":\"{0}\",'.format(seller_account))
