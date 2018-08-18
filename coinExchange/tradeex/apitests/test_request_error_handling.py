4fc45678d55523e27bc864a301a00f68e391f28f
Tao <taozhang@salesforce.com>
2018-08-18 11:38:55 +0200

    fix error handle of long notify response

--- coinExchange/tradeex/apitests/test_request_error_handling.py
+++ coinExchange/tradeex/apitests/test_request_error_handling.py
@@ -24,6 +24,7 @@ from tradeex.controllers.crypto_utils import *
 from tradeex.models import *
 from trading.models import *
 from trading.controller import useraccountinfomanager
+from trading.controller import ordermanager
 import json
 
 TEST_HY_BILL_NO='180102122300364021000081666'
@@ -43,6 +44,16 @@ TEST_REDEEM_AMOUNT = 50
 TEST_CNY_ADDR="TRADDEX_USER1_EXTERNAL_TEST_ADDR"
 TEST_CRYPTO_SEND_COMMENT = ""
 TEST_NOTIFY_URL = "http://testurl/"
+
+
+RECEIVE_ACCOUNT_NOT_EXIST='{"sign": "D083D11085B23E3D2136315FEEDFA18F", "return_msg": "收钱方账号不存在", "return_code": "FAIL"}'
+def send_buy_apply_with_bad_account_error_side_effect(payload):
+    return 200, 'Ok', RECEIVE_ACCOUNT_NOT_EXIST
+
+def send_notify_for_bad_account_get_long_response_side_effect(payload, trackId='', response_format='json'):
+    with open('long_notify_response.txt', 'r') as myfile:
+        response_str=myfile.read()
+    return response_str
 
 class TestErrorHandling(TransactionTestCase):
     fixtures = ['fixture_test_tradeapi.json']
@@ -222,4 +233,63 @@ class TestErrorHandling(TransactionTestCase):
         self.assertEqual(200, response.status_code)
         resp_json = json.loads(response.content.decode('utf-8'))
         self.assertEqual(resp_json['return_code'], 'FAILED')
-        self.assertEqual(resp_json['return_msg'], '缺失支付方式或提供的支付方式系统不支持')        
+        self.assertEqual(resp_json['return_msg'], '缺失支付方式或提供的支付方式系统不支持')
+
+    def test_purchase_skip_sell_order_with_bad_account(self):
+        pass
+
+    def test_redeem_with_notify_of_bad_account(self):
+        pass
+
+    @patch('trading.controller.heepaymanager.HeePayManager.send_buy_apply_request', 
+           side_effect=send_buy_apply_with_bad_account_error_side_effect)
+    @patch('tradeex.client.apiclient.APIClient.send_json_request', 
+            side_effect=send_notify_for_bad_account_get_long_response_side_effect)
+    def test_redeem_with_notify_of_bad_account_with_failure_response(self, send_json_request_function, send_buy_apply_request_function):
+        resp = create_axfund_sell_order('tttzhang2000@yahoo.com', 'user@123', 2, 0.5, 'CNY')
+        request = TradeAPIRequest(
+                API_METHOD_REDEEM,
+                TEST_API_USER2_APPKEY,
+                TEST_API_USER2_SECRET,
+                'order_no_order', # order id
+                None, # trx_id
+                500, # total fee
+                10, # expire_minute
+                'heepay', '12345',
+                '127.0.0.1', #client ip
+                attach='userid:1',
+                subject='人民币提现测试-无效付款账号',
+                notify_url='http://testurl',
+                return_url='http://retururl')
+        c = Client()
+        request_json = json.loads(request.getPayload())
+        request_str = json.dumps(request_json, ensure_ascii=False)
+        response = c.post('/api/v1/applyredeem/', request_str,
+                          content_type='application/json')
+
+        try:
+            redeem_order = Order.objects.get(order_type='SELL', order_source='API')
+        except:
+            self.fail('Could not get the only redeem sell order {0}'.format(sys.exc_info[0])) 
+
+        request = TradeAPIRequest(
+                API_METHOD_PURCHASE,
+                TEST_API_USER1_APPKEY,
+                TEST_API_USER1_SECRET,
+                'order_no_order', # order id
+                None, # trx_id
+                10000, # total fee
+                10, # expire_minute
+                'heepay', '23456',
+                '127.0.0.1', #client ip
+                attach='userid:1',
+                subject='购买无效付款账号的提现订单',
+                notify_url='http://testurl',
+                return_url='http://retururl')
+        c = Client()
+        request_json = json.loads(request.getPayload())
+        request_str = json.dumps(request_json, ensure_ascii=False)
+        response = c.post('/api/v1/applypurchase/', request_str,
+                          content_type='application/json')
+        
+                
