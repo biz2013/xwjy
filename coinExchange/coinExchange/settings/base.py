bec1bd1197e530245eae25308d7a04e3a6218c9a
Tao <taozhang@salesforce.com>
2018-09-23 14:52:00 -0700

    fix the problem that confirm trans stop notification

--- coinExchange/coinExchange/settings/base.py
+++ coinExchange/coinExchange/settings/base.py
@@ -146,8 +146,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
