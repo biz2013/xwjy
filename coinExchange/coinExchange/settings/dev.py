4ef03a250def3f72a0d9bbe5c0f16c077a83d3b1
Tao <taozhang@salesforce.com>
2018-10-06 22:44:33 -0700

    let cron job use setting to judge incoming ip

--- coinExchange/coinExchange/settings/dev.py
+++ coinExchange/coinExchange/settings/dev.py
@@ -1,6 +1,5 @@
 from .base import *
 
-ALLOWED_HOSTS = ['54.218.104.21', 'localhost', '127.0.0.1']
 # send email to console
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
@@ -8,6 +7,6 @@ STATIC_ROOT = "/var/www/coinexchange/static/"
 
 MEDIA_ROOT = "/var/www/coinexchange/media/"
 
-ALLOWED_HOSTS = ['54.203.195.52', '172.31.0.229', 'ip-172-31-0-229.us-west-2.compute.internal', 'localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['52.43.117.129', '172.31.0.229', 'ip-172-31-0-229.us-west-2.compute.internal', 'localhost', '127.0.0.1', '[::1]']
 
 API_TRANS_LIMIT = 10
