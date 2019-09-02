4792357028954ece211eaacc04be5075508601bb
Ubuntu <ubuntu@ip-172-31-8-165.us-west-2.compute.internal>
2019-09-02 00:37:22 +0000

    fix code that break the list trans

--- coinExchange/coinExchange/settings/base.py
+++ coinExchange/coinExchange/settings/base.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
         'USER': 'mydbuser',
-        'PASSWORD': 'AXFund@019',
+        'PASSWORD': 'aaaaaa',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'TEST':{
